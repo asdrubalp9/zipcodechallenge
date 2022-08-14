915addb2f25faea838a68b50bf8252b82548a5f5
Asdrubal Nitro <asdrubalp9@gmail.com>
2022-08-14 16:31:52 -0400

    fix controlador

--- app/Http/Controllers/Api/V1/ZipsController.php
+++ app/Http/Controllers/Api/V1/ZipsController.php
@@ -9,42 +9,43 @@ class ZipsController extends Controller
 {
     public function index($zipcode, $statusCode = 500)
     {
-
-        $entities =  DB::select('SELECT
-                        `d_codigo`, `d_asenta`, `d_tipo_asenta`, `D_mnpio`, `d_estado`, `d_ciudad`, `d_CP`, `c_estado`, `c_oficina`, `c_CP`, `c_tipo_asenta`, `c_mnpio`, `id_asenta_cpcons`, `d_zona`, `c_cve_ciudad`
-                    FROM entities
-                        WHERE 
-                    d_codigo = ?
-                    ', [$zipcode]);
-
         $data = [
             'message' => 'No results found',
         ];
+        if ($zipcode) {
 
-        if (count($entities) > 0) {
-            $statusCode = 200;
-            $data = [
-                "zip_code" => $entities[0]->d_codigo,
-                "locality" => $entities[0]->d_estado,
-                "federal_entity" => [
-                    "key" => $entities[0]->c_estado,
-                    "name" => $entities[0]->d_estado,
-                    "code" => null
-                ],
-                "municipality" => [
-                    "key" => 10,
-                    "name" => $entities[0]->D_mnpio
-                ]
-            ];
-            foreach ($entities as $entitie) {
-                $data['settlements'][] = [
-                    "key" => $entitie->id_asenta_cpcons,
-                    "name" => $entitie->d_asenta,
-                    "zone_type" => $entitie->d_zona,
-                    "settlement_type" => [
-                        "name" => $entitie->d_tipo_asenta
+            $entities =  DB::select('SELECT
+                            `d_codigo`, `d_asenta`, `d_tipo_asenta`, `D_mnpio`, `d_estado`, `d_ciudad`, `d_CP`, `c_estado`, `c_oficina`, `c_CP`, `c_tipo_asenta`, `c_mnpio`, `id_asenta_cpcons`, `d_zona`, `c_cve_ciudad`
+                        FROM entities
+                            WHERE 
+                        d_codigo = ?
+                        ', [$zipcode]);
+
+            if (count($entities) > 0) {
+                $statusCode = 200;
+                $data = [
+                    "zip_code" => $entities[0]->d_codigo,
+                    "locality" => $entities[0]->d_estado,
+                    "federal_entity" => [
+                        "key" => $entities[0]->c_estado,
+                        "name" => $entities[0]->d_estado,
+                        "code" => null
+                    ],
+                    "municipality" => [
+                        "key" => 10,
+                        "name" => $entities[0]->D_mnpio
                     ]
                 ];
+                foreach ($entities as $entitie) {
+                    $data['settlements'][] = [
+                        "key" => $entitie->id_asenta_cpcons,
+                        "name" => $entitie->d_asenta,
+                        "zone_type" => $entitie->d_zona,
+                        "settlement_type" => [
+                            "name" => $entitie->d_tipo_asenta
+                        ]
+                    ];
+                }
             }
         }
 
