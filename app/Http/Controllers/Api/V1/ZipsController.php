503985f9f36c7212e3e2e5dac8aa373e29589563
Asdrubal Nitro <asdrubalp9@gmail.com>
2022-08-14 16:47:02 -0400

    fixes

--- app/Http/Controllers/Api/V1/ZipsController.php
+++ app/Http/Controllers/Api/V1/ZipsController.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\DB;
 
 class ZipsController extends Controller
 {
+    private function stripAccents($str)
+    {
+        return strtr(utf8_decode($str), utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'), 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
+    }
     public function index($zipcode, $statusCode = 500)
     {
         $data = [
@@ -27,20 +31,20 @@ class ZipsController extends Controller
                     "zip_code" => $entities[0]->d_codigo,
                     "locality" => $entities[0]->d_estado,
                     "federal_entity" => [
-                        "key" => $entities[0]->c_estado,
-                        "name" => $entities[0]->d_estado,
+                        "key" => (int) $entities[0]->c_estado,
+                        "name" => strtoupper($this->stripAccents($entities[0]->d_estado)),
                         "code" => null
                     ],
                     "municipality" => [
-                        "key" => 10,
-                        "name" => $entities[0]->D_mnpio
+                        "key" => (int)$entities[0]->c_mnpio,
+                        "name" => strtoupper($this->stripAccents($entities[0]->D_mnpio))
                     ]
                 ];
                 foreach ($entities as $entitie) {
                     $data['settlements'][] = [
                         "key" => $entitie->id_asenta_cpcons,
-                        "name" => $entitie->d_asenta,
-                        "zone_type" => $entitie->d_zona,
+                        "name" => strtoupper($this->stripAccents($entitie->d_asenta)),
+                        "zone_type" => strtoupper($entitie->d_zona),
                         "settlement_type" => [
                             "name" => $entitie->d_tipo_asenta
                         ]
